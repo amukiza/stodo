7ad15e9b83d872f1547855524c57f833245448af
Andrew Mukiza <andrewmukiza@gmail.com>
2014-03-08 15:31:17 +0300

    adding task and taskmanager classes

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -3,7 +3,8 @@ require File.expand_path '../../lib/task.rb', __FILE__
 
 describe "Task" do
     describe "Task With data" do
-        let(:task){Task.new "name", "Description", "2006-09-05"}
+    let(:task){Task.new "name", "Description", "2006-09-05"}
+    describe "Task With data" do
         it { task.name.should_not be_nil}
         it { task.description.should_not be_nil}
         it { task.date.should_not be_nil}
