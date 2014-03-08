22bca864958763d2f93a12ceccc0d3dd6b947d01
Andrew Mukiza <andrewmukiza@gmail.com>
2014-03-08 16:48:54 +0300

    now simmilar tasks are considered equal

--- lib/task.rb
+++ lib/task.rb
@@ -1,9 +1,17 @@
 class Task
   attr_accessor :date, :description, :name
 
-  def initialize(date, description, name)
-    @date = date
-    @description = description
+  def initialize(name, description, date)
     @name = name
+    @description = description
+    @date = date
+  end
+  
+  def ==(o)
+      o.class == self.class && self.state == o.state
+  end
+  
+  def state
+      [@name, @description, @date]
   end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -3,10 +3,20 @@ require File.expand_path '../../lib/task.rb', __FILE__
 
 describe "Task" do
     describe "Task With data" do
-    let(:task){Task.new "name", "Description", "2006-09-05"}
-    describe "Task With data" do
+      let(:task){Task.new "name", "Description", "2006-09-05"}
+      describe "Task With data" do
         it { task.name.should_not be_nil}
         it { task.description.should_not be_nil}
         it { task.date.should_not be_nil}
     end
+
+    describe "Task equality" do
+        it "Should be equal if given simmilar values" do
+            expect(task).to eq(Task.new "name", "Description", "2006-09-05")
+        end
+        it "should not be equal if some attributes differ" do
+            expect(task).not_to eq(Task.new "cooking beans", "hmmmm", "2006-09-11")
+        end
+    end
+  end
 end
